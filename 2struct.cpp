792e91dc3a6ffe973abf9707fb7593315b8a31b8
rishikesh835 <rishikeshmishra835@gmail.com>
2023-10-28 00:00:55 +0530

    added constructer to struct

--- 2struct.cpp
+++ 2struct.cpp
@@ -1,24 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef struct node{
-string str;
-int num;
-double doub;
-char x;
-node(string s,int n,double d,char c){
-    str=s;
-    num=n;
-    doub=d;
-    x=c;
-}
+typedef struct node
+{
+    string str;
+    int num;
+    double doub;
+    char x;
+    node(void) {}
+    node(string s, int n, double d, char c)
+    {
+        str = s;
+        num = n;
+        doub = d;
+        x = c;
+    }
 
-}node;
-int main(){
+} node;
+int main()
+{
     node n1;
-    n1.str="rishi";
-    n1.num=5;
-    n1.doub=10.8;
-    n1.x='r';
-    node *raj=new node("striver",14,4.5,'c');
-    node rishi=node("rishi",12,1.2,'c');
+    n1.str = "rishi";
+    n1.num = 5;
+    n1.doub = 10.8;
+    n1.x = 'r';
+    node *raj = new node("striver", 14, 4.5, 'c');
+    node rishi = node("rishi", 12, 1.2, 'c');
 }
